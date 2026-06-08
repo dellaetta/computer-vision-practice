a84aecb7e967fe713f1b39398ec7bcd7f750a868
chloev <138642390+dellaetta@users.noreply.github.com>
2026-06-08 19:22:27 -0400

    take picture with camera

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,18 @@
 import cv2
-cam = cv2.VideoCapture(0) # 0 = first webcam, 1 = second webcam
+import logging
+import time
+
+cam = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION) # 0 = first webcam, 1 = second webcam
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+ret, frame = cam.read()
+
+if ret:
+        cv2.imshow("Display", frame)
+        cv2.waitKey(10000)
+else:
+    logging.exception(" An exception occurred while capturing image")
+
+cam.release()
+cv2.destroyAllWindows()
