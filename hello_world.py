307841a3fe4fe36faa17dd056f7f29038db1ab8c
chloev <138642390+dellaetta@users.noreply.github.com>
2026-06-08 19:32:59 -0400

    added hello world

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,19 @@
 import cv2
 import logging
-import time
 
 cam = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION) # 0 = first webcam, 1 = second webcam
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-ret, frame = cam.read()
+# repeat to allow camera to warm up
+for _ in range(3):
+    ret, frame = cam.read()
 
 if ret:
+        # ?, text, position (x,y), font scale, color, thickness
+        cv2.putText(frame, "Hello World!", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (199,21,133), 3) 
         cv2.imshow("Display", frame)
         cv2.waitKey(10000)
 else:
-    logging.exception(" An exception occurred while capturing image")
+    logging.error("Image not captured")
 
 cam.release()
 cv2.destroyAllWindows()
