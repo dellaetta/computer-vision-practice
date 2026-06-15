b2f21a0b5af086bb24329fc3fcb83c8bcd2c740e
chloev <138642390+dellaetta@users.noreply.github.com>
2026-06-15 12:52:36 -0400

    building out live feed

--- chap_3/live_cam.py
+++ chap_3/live_cam.py
@@ -3,18 +3,20 @@ import logging
 
 cam = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION)
 mouse_x, mouse_y = 0, 0
+clicked_x, clicked_y = None, 0
 current_frame = None
 b, g, r = 0, 0, 0
 WINDOW = "Live Camera"
 
 def mouse_callback(event, x, y, flags, param):
     global mouse_x, mouse_y
+    global clicked_x, clicked_y
     global b, g, r
     global current_frame
 
+    mouse_x, mouse_y = x, y
     if event == cv2.EVENT_LBUTTONDOWN:
-        mouse_x, mouse_y = x, y
-
+        clicked_x, clicked_y = x, y
         b, g, r = current_frame[y, x]
     
 
@@ -29,25 +31,12 @@ while True:
 
     current_frame = frame
 
-    cv2.putText(
-        frame,
-        f"pos: ({mouse_x}, {mouse_y})",
-        (10, 30),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 255, 0),
-        2
-    )
+    cv2.putText(frame, f"mouse: ({mouse_x}, {mouse_y})", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+    cv2.putText(frame, f"clicked: ({clicked_x if clicked_x != None else 0}, {clicked_y})", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+    cv2.putText(frame, f"rgb: ({r}, {g}, {b})", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
 
-    cv2.putText(
-        frame,
-        f"rgb: ({r}, {g}, {b})",
-        (10, 70),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 255, 0),
-        2
-    )
+    if clicked_x != None:
+        cv2.circle(frame, (clicked_x, clicked_y), 10, (0, 0, 255), thickness=-1)
 
     cv2.imshow(WINDOW, frame)
 
