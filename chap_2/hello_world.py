0881b04d9f74311ea9c6a87a46c81ed3b40d93ee
chloev <138642390+dellaetta@users.noreply.github.com>
2026-06-15 13:59:07 -0400

    added photo booth

--- chap_2/hello_world.py
+++ chap_2/hello_world.py
@@ -4,14 +4,14 @@ import logging
 cam = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION) # 0 = first webcam, 1 = second webcam
 
 # repeat to allow camera to warm up
-for _ in range(3):
+for x in range(3):
     ret, frame = cam.read()
 
 if ret:
-        # ?, text, position (x,y), font scale, color, thickness
-        cv2.putText(frame, "Hello World!", (50, 100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (199,21,133), 3) 
-        cv2.imshow("Display", frame)
-        cv2.waitKey(10000)
+    # ?, text, position (x,y), font scale, color, thickness
+    cv2.putText(frame, "Hello World!", (50, 100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (199,21,133), 3) 
+    cv2.imshow("Display", frame)
+    cv2.waitKey(10000)
 else:
     logging.error("Image not captured")
 
---
+++ chap_2/photo_booth.py
@@ -0,0 +1,49 @@
+import cv2
+import time
+import logging
+import random
+
+cam = cv2.VideoCapture(0)
+WINDOW = "Display"
+cv2.namedWindow(WINDOW)
+rand_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+counter = 0
+
+# Inital image
+for x in range(3):               
+    ret, frame = cam.read()
+    if ret:
+        if x == 2:
+            cv2.putText(frame, "Left click to save a photo", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, rand_color, 2) 
+            cv2.imshow(WINDOW, frame)
+            cv2.waitKey(3000)
+    else: 
+        logging.error("Photo not caputred")
+
+# Left click definition
+def mouse_callback(event, x, y, flags, param):
+    global counter
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        filepath = "photos/photo_booth/image-" + str(counter) + ".jpg"
+        cv2.imwrite(filepath, frame)
+        print(f"Saved photo_{counter} to {filepath}")
+        counter = counter + 1
+
+cv2.setMouseCallback(WINDOW, mouse_callback)  
+
+# Live
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        logging.error("Photo not caputred")
+        break
+    
+    cv2.putText(frame, f"Photos taken: {counter}", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, rand_color, 2)
+    cv2.imshow(WINDOW, frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
